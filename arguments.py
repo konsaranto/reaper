66c43b2317d4d8b10110059daebfccce3fbc931e
Konstantinos Sarantopoulos <konsaranto@gmail.com>
2018-10-17 00:21:46 +0300

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -33,8 +33,8 @@ def arguments(*args):
         #display help
         if args[i] == "-h":
             help = """Syntax:	reaper.py	[-u username | -U username_file][-p password | -P password_file][-url url | -URL url_file]
-                		[-f save_file][-t number_of_tries:time_to_wait][-c url_after_failed_attempt][-th number_of_threads]
-                		[-s][-up file_with_credentials][parameters]
+			[-f save_file][-t number_of_tries:time_to_wait][-c url_after_failed_attempt][-th number_of_threads]
+			[-s][-up file_with_credentials][parameters]
 
 Flags:	-h display the help section
 	-u set the username
