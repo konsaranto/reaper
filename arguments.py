b53153a0b714682906069c352e9b412d83997b9d
Kostas <konsaranto@gmail.com>
2018-12-23 01:21:06 +0200

    update

--- arguments.py
+++ arguments.py
@@ -31,25 +31,25 @@ def arguments(*args):
     threadlist = ['Thread-1']
     for i in range(0, len(args)):
         #display help
-        if args[i] == "-h":
-            help = """Syntax: reaper.py 	[-u username | -U username_file][-p password | -P password_file]
-	    		[-url url | -URL url_file][-f save_file][-t number_of_tries:time_to_wait][-c url_after_failed_attempt]
-			[-th number_of_threads][-s][-up file_with_credentials][parameters]
+        if args[i] == "-h" or args[i] == "--help":
+            help = """Syntax: reaper.py     [-u username | -U username_file][-p password | -P password_file]
+                [-url url | -URL url_file][-f save_file][-t number_of_tries:time_to_wait][-c url_after_failed_attempt]
+            [-th number_of_threads][-s][-up file_with_credentials][parameters]
 
-Flags:	-h display the help section
-	-u set the username
-	-U set the file containing the usernames (one username at each line)
-      	-p set the password
-      	-P set the file containing the passwords (one password at each line)
-      	-up set the file containing the credentials (Syntax username:password) (one combination at each line)
-	-url set the url and the port (e.g. http://127.0.0.1:160/login.php or http://scanme.nmap.org)
-	 *don't omit /login.php or whatever it is, if it exists, and don't omit http:// or https://
-      	-URL set the file containing the urls you want to attack (one url at each line)
-	-f set the file to save the valid credentials found 						(required)
-	-t set the number x of tries before waiting for y seconds (Syntax x:y) 				(optional)
-      	-c set the full (http://) url that loads when you make a failed attempt
-      	-th set the number of threads									(optional)
-      	-s use a second method of validating credentials						(optional)"""
+Flags:    -h display the help section
+    -u set the username
+    -U set the file containing the usernames (one username at each line)
+          -p set the password
+          -P set the file containing the passwords (one password at each line)
+          -up set the file containing the credentials (Syntax username:password) (one combination at each line)
+    -url set the url and the port (e.g. http://127.0.0.1:160/login.php or http://scanme.nmap.org)
+     *don't omit /login.php or whatever it is, if it exists, and don't omit http:// or https://
+          -URL set the file containing the urls you want to attack (one url at each line)
+    -f set the file to save the valid credentials found                         (required)
+    -t set the number x of tries before waiting for y seconds (Syntax x:y)                 (optional)
+          -c set the full (http://) url that loads when you make a failed attempt
+          -th set the number of threads                                    (optional)
+          -s use a second method of validating credentials                        (optional)"""
             print (help)
             sys.exit()
 
@@ -63,33 +63,33 @@ Flags:	-h display the help section
             url = urlfile.read().split("\n")
             url = url[:-1]
             for j in range(0,len(url)):
-            	for k in url:
-            		if k == "":
-            			url.remove(k)
+                for k in url:
+                    if k == "":
+                        url.remove(k)
             urlfile.close()
 
         #file to save results
         if args[i] == "-f":
-        	save = args[i+1]
+            save = args[i+1]
 
         #read the username
         if args[i] == "-u":
-        	usernames = args[i+1].split()
+            usernames = args[i+1].split()
 
         #read the password
         if args[i] == "-p":
-        	passwords = args[i+1].split()
+            passwords = args[i+1].split()
 
         #read and split the credentials file
         if args[i] == "-up":
-        	creds = open(args[i+1], "r")
-        	credentials = creds.read().split("\n")
-        	credentials = credentials[:-1]
-        	for j in range(0,len(credentials)):
-        		for k in credentials:
-        			if k == "":
-        				credentials.remove(k)
-        	creds.close()
+            creds = open(args[i+1], "r")
+            credentials = creds.read().split("\n")
+            credentials = credentials[:-1]
+            for j in range(0,len(credentials)):
+                for k in credentials:
+                    if k == "":
+                        credentials.remove(k)
+            creds.close()
 
         #read and split the username file
         if args[i] == "-U":
@@ -99,7 +99,7 @@ Flags:	-h display the help section
             for j in range(0,len(usernames)):
                 for k in usernames:
                        if k == "":
-        	                 usernames.remove(k)
+                             usernames.remove(k)
             userfile.close()
 
         #read and split the password file
@@ -113,39 +113,11 @@ Flags:	-h display the help section
                         passwords.remove(k)
             passfile.close()
 
-        #get the parameters and make them a list
-        if re.search("=.*?/", args[i]):
-            parameters_list = []
-            #if there is a credentials file
-            for c in range(0, len(args)):
-                if args[c] == "-up":
-                	for j in range(0,len(credentials)):
-                		#split the credentials
-                		creds = credentials[j].split(":")
-                		#replace USERNAME and PASSWORD with the credentials
-                		parameters = args[i].replace('USERNAME', creds[0])
-                		parameters = parameters.replace('PASSWORD', creds[1])
-                		parameters = parameters.split()
-                		#extend the param list with the parameters
-                		parameters_list.extend(parameters)
-                	break
-            #if the usernames and passwords are given separately
-            for c in range(0, len(args)):
-                if args[c] in ["-u", "-U"]:
-                    for username in usernames:
-                    	for password in passwords:
-                    		#replace USERNAME and PASSWORD
-                    		parameters = args[i].replace('USERNAME', username)
-                    		parameters = parameters.replace('PASSWORD', password)
-                    		parameters = parameters.split()
-                    		#extend the param list with the parameters
-                    		parameters_list.extend(parameters)
-
         #set the sleeping variable
         if args[i] == "-t":
-        	timing = args[i+1].split(":")
-        	number_of_tries = timing[0]
-        	time_to_sleep = timing[1]
+            timing = args[i+1].split(":")
+            number_of_tries = timing[0]
+            time_to_sleep = timing[1]
 
         #if the page changes with failed attempts
         if args[i] == "-c":
@@ -153,11 +125,41 @@ Flags:	-h display the help section
 
         #if the second way of validating credentials is enabled
         if args[i] == "-s":
-        	second_way = 1
+            second_way = 1
 
         #set the number of threads
         if args[i] == "-th":
-        	for j in range(1, int(args[i+1])):
-        		threadlist.append("Thread-%s" % (j + 1))
+            for j in range(1, int(args[i+1])):
+                threadlist.append("Thread-%s" % (j + 1))
+
+    #last, after all variables have been set
+    for i in range(0, len(args)):
+        #get the parameters and make them a list
+        if re.search("=.*/", args[i]):
+            parameters_list = []
+            #if there is a credentials file
+            for c in range(0, len(args)):
+                if args[c] == "-up":
+                    for j in range(0,len(credentials)):
+                        #split the credentials
+                        creds = credentials[j].split(":")
+                        #replace USERNAME and PASSWORD with the credentials
+                        parameters = args[i].replace('USERNAME', creds[0])
+                        parameters = parameters.replace('PASSWORD', creds[1])
+                        parameters = parameters.split()
+                        #extend the param list with the parameters
+                        parameters_list.extend(parameters)
+                    break
+            #if the usernames and passwords are given separately
+            for c in range(0, len(args)):
+                if args[c] in ["-u", "-U"]:
+                    for username in usernames:
+                        for password in passwords:
+                            #replace USERNAME and PASSWORD
+                            parameters = args[i].replace('USERNAME', username)
+                            parameters = parameters.replace('PASSWORD', password)
+                            parameters = parameters.split()
+                            #extend the param list with the parameters
+                            parameters_list.extend(parameters)
 
     return save, url, parameters_list, number_of_tries, time_to_sleep, page, second_way, threadlist
